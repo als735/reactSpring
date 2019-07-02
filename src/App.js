bd00a0db5988255ec8cfc087ae2bfd599225343e
Abby Smith <smith@Abbys-MacBook-Pro.local>
2019-07-01 20:57:20 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import UseSpring from './UseSpring'; 
+// import {useSpring, animated} from 'react-spring'
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <UseSpring/>
+
     </div>
   );
 }
---
+++ src/UseSpring.js
@@ -0,0 +1,26 @@
+// import {useSpring, animated} from 'react-spring'
+import React, { Component } from 'react'
+
+
+
+ class UseSpring extends Component {
+    // constructor(props){
+    //     super(props); 
+    // }
+    
+
+    render() {
+        // const props = useSpring({opacity: 1, from: {opacity: 0}})
+        // <animated.div style={props}>I will fade in</animated.div>
+
+        return (
+            <div>
+                Hello Brent! 
+
+            </div>
+        )
+    }
+}
+
+
+export default UseSpring; 
