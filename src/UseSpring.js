54543ce84637f1204960a70bfd17f76793b87160
Abby Smith <smith@Abbys-MacBook-Pro.local>
2019-07-01 23:32:09 -0600

    numbers added

--- src/UseSpring.js
+++ src/UseSpring.js
@@ -1,17 +1,33 @@
 // import {useSpring, animated} from 'react-spring'
 import React from 'react'
 import { useSpring, animated } from 'react-spring';
-    
+
 
 const HookedComponent = () => {
     const props = useSpring({
         opacity: 9,
-        color: 'yellow',
+        color: 'red',
         from: { opacity: 0 },
-        delay: '2000'
+        delay: '6000'
     })
-    return <animated.div style={props}>This text Faded in Using hooks</animated.div>
+    return <animated.div style={props} className='Hooked'>This text Faded in Using hooks</animated.div>
 }
 
- 
-export default HookedComponent;
+const HookedComponentTwo = () => {
+    const props = useSpring({
+        opacity: 9,
+        color: 'red',
+        from: { opacity: 0 },
+        delay: '1000'
+    })
+    return <animated.div style={props} className='HookedTwo'>This text Faded in Using hooks</animated.div>
+}
+
+const HookedComponentThree = () => {
+    const props = useSpring({ number: 1, from: { number: 0 } })
+return <animated.span>{props.number}</animated.span>
+   
+}
+  
+
+export default (HookedComponent, HookedComponentTwo, HookedComponentThree);
